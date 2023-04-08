c9ad9d18141c64c11a7d8424794b3e3d48bf3c9f
QWQAA <2455069465@qq.com>
2023-04-08 21:37:30 +0800

    second commit

--- Myweb/whhs/urls.py
+++ Myweb/whhs/urls.py
@@ -4,8 +4,10 @@ from . import views  # 渲染界面
 # app_name = 'whhs'
 # 添加路径
 urlpatterns = [
-    path('', views.toLogin_view, name='login'),   # 路径控制
-    path('index/', views.toIndex_view, name='index'),
-
+    # 路径控制
+    path('101/', views.to_register, name='register'),    # 注册界面
+    path('register/', views.register_view),          # 注册逻辑处理地址
+    path('102/', views.toLogin_view, name='login'),      # 登录界面
+    path('index/', views.toIndex_view, name='index'),  # 登录逻辑处理
 
 ]
--- Myweb/whhs/views.py
+++ Myweb/whhs/views.py
@@ -15,13 +15,31 @@ def toLogin_view(request):
     return render(request, 'login.html')   # 返回界面
 
 
+def to_register(request):
+    return render(request, 'register.html')
+
+
+def register_view(request):
+    u = request.POST.get("user", '')
+    p = request.POST.get("pwd", '')
+
+    if u and p:
+        c = StudentInfo.objects.filter(stu_user=u, stu_pwd=p).count()
+        if c == 1:
+            return HttpResponse("用户存在")
+        else:
+            return HttpResponse("登录成功")
+    else:
+        return HttpResponse("请输入账号密码")
+
+
 def toIndex_view(request):
     u = request.POST.get("user", '')
     p = request.POST.get("pwd", '')
 
     if u and p:
         c = StudentInfo.objects.filter(stu_user=u, stu_pwd=p).count()
-        if c >=1:
+        if c == 1:
             return HttpResponse("登录成功")
         else:
             return HttpResponse("登录失败")
